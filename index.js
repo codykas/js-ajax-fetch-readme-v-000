cda11f5a0b5eefd2de38639ae61cb7676f7ddf7d
Cody Kasselman <ckasselman@fordham.edu>
2017-10-31 01:33:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const app = "I don't do much.";
+const token = '321b5d12c038bfd474eedd60f496075bca6aebd7'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
